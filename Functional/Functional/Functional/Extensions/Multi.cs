fafabd26b95756026aaa298e7688133012add88d
Adam Pedley <adam.pedley@gmail.com>
2018-03-11 10:45:13 +1100

    New Multi Markup Extension

---
+++ Functional/Functional/Functional/Extensions/Multi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Functional.Extensions
+{
+	[ContentProperty("Items")]
+	public class MultiExtension : IMarkupExtension<Array>
+	{
+		public MultiExtension()
+		{
+			Items = new List<object>();
+		}
+
+		public IList Items { get; }
+
+		public string[] Text { get; set; }
+
+		public object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return (this as IMarkupExtension<Array>).ProvideValue(serviceProvider);
+			
+		}
+
+		Array IMarkupExtension<Array>.ProvideValue(IServiceProvider serviceProvider)
+		{
+			var rootObject = serviceProvider.GetService(typeof(IRootObjectProvider)) as IRootObjectProvider;
+			var page = (rootObject.RootObject as Page);
+
+			var snapshot = new List<Data>();
+			foreach (var item in Items)
+			{
+				snapshot.Add(new Data() { Text = page.FindByName<Entry>((string)item) });
+			}
+
+			return snapshot.ToArray();
+		}
+	}
+}
